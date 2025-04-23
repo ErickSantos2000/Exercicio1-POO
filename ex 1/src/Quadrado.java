0df402a387ac42f9b03a4a3b4f6a1a698a602e9b
Erick Felipe <devErickFelipe@gmail.com>
2025-04-23 16:07:11 -0300

    Update Quadrado.java

--- ex 1/src/Quadrado.java
+++ ex 1/src/Quadrado.java
@@ -1,6 +1,10 @@
 public class Quadrado{
     private int lado;
 
+    public Quadrado(int lado){
+        this.lado = lado;
+    }
+
     public void setLado(int lado){
         this.lado = lado;
     }
