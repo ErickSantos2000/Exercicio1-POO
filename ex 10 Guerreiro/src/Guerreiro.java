7a37ee74e0efb54e2e2216f81695fe3eb449de53
Erick Felipe <devErickFelipe@gmail.com>
2025-05-15 10:52:15 -0300

    Update Guerreiro.java

--- ex 10 Guerreiro/src/Guerreiro.java
+++ ex 10 Guerreiro/src/Guerreiro.java
@@ -65,6 +65,78 @@ public class Guerreiro {
 
     }
 
+    public void lutaInterativa(Guerreiro oponente){
+        Random random = new Random();
+        int rodada = 0;
+
+        while (this.getEnergia() != 0 && oponente.getEnergia() != 0){
+            rodada++;
+            System.out.println("---Rodada " + rodada + "------------");
+            this.atacar(oponente);
+            oponente.atacar(this);
+            int numeroAleatorio = random.nextInt(2);
+
+            if(numeroAleatorio == 1){
+                this.alimentaSe();
+            }
+            else {
+                oponente.alimentaSe();
+            }
+
+            System.out.println();
+
+            System.out.println("Enegia de " + this.getNome()+ " "+ this.getEnergia());
+            System.out.println("Enegia de " + oponente.getNome()+ " "+ oponente.getEnergia());
+            System.out.println("------------------------");
+
+        }
+
+        if(this.getEnergia() == 0){
+            System.out.println(oponente.getNome() + " ganhou!");
+        }
+        else{
+            System.out.println(this.getNome() + " ganhou!");
+        }
+    }
+
+    public void LutaRecursiva(Guerreiro oponente) {
+        Random random = new Random();
+        int rodada = 0;
+    
+        
+ 
+        System.out.println("---Rodada " + rodada + "------------");
+        this.atacar(oponente);
+        oponente.atacar(this);
+    
+        int numeroAleatorio = random.nextInt(2);
+        if (numeroAleatorio == 1) {
+            this.alimentaSe();
+        } else {
+            oponente.alimentaSe();
+        }
+    
+        System.out.println();
+        System.out.println("Energia de " + this.getNome() + ": " + this.getEnergia());
+        System.out.println("Energia de " + oponente.getNome() + ": " + oponente.getEnergia());
+        System.out.println("------------------------");
+
+
+        if (this.getEnergia() == 0) {
+            System.out.println(oponente.getNome() + " ganhou!");
+            return;
+        }
+    
+        if (oponente.getEnergia() == 0) {
+            System.out.println(this.getNome() + " ganhou!");
+            return;
+        }
+    
+    
+        LutaRecursiva(oponente);
+    }
+    
+    
 
     public String toString() {
         return String.format("Nome: %s\nEnergia: %d\nCodigo: %d", nome, energia, codigo);
